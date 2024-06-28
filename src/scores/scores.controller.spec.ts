15924c014082a7b86d46d75b1648387451f2080e
putraasmarjoe <putraasmarjoe@gmail.com>
2024-06-29 01:51:39 +0800

    update score controller test

--- src/scores/scores.controller.spec.ts
+++ src/scores/scores.controller.spec.ts
@@ -1,10 +1,10 @@
-// scores.controller.spec.ts
 import { Test, TestingModule } from '@nestjs/testing';
 import { ScoresController } from './scores.controller';
 import { ScoresService } from './scores.service';
 import { DatabaseService } from '../database/database.service';
-import { ConfigModule } from '@nestjs/config'; // Ensure ConfigModule is imported
+import { ConfigModule } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
+import { RateLimiterModule } from 'nestjs-rate-limiter';
 
 describe('ScoresController', () => {
   let controller: ScoresController;
@@ -12,11 +12,12 @@ describe('ScoresController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [
-        ConfigModule.forRoot(), // Import ConfigModule
+        ConfigModule.forRoot(),
         JwtModule.register({
           secret: 'test-secret',
           signOptions: { expiresIn: '1h' },
         }),
+        RateLimiterModule, // Import RateLimiterModule directly
       ],
       controllers: [ScoresController],
       providers: [ScoresService, DatabaseService],
